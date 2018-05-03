b9dee8092827fc8148314c8f01dd5a9a97fcf963
Peter Jackson <pjackson@students.mchenry.edu>
2018-05-02 20:03:34 -0500

    Fixed crash on summary screen

--- app/src/main/java/com/example/pjackson/groceryapp/DisplaySummary.java
+++ app/src/main/java/com/example/pjackson/groceryapp/DisplaySummary.java
@@ -208,7 +208,6 @@ public class DisplaySummary extends AppCompatActivity {
         }
         itemsBoughtSummary.setText(ItemsBought);
 
-
         StringBuilder ItemsRemain = new StringBuilder(500);
         if (itemR1 == null) {
             ItemsRemain.append(buy);
